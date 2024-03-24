a49e87f39cf3ae53529bc4c51c421e73d991d215
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-03-24 17:35:26 -0400

    Add Stretch Features: Rounded Image Corners and Shared Element Transition
    - Add Rounded corners to media poster when clicked and on detail screen
    - Add shared element transition to media poster and title

--- app/src/main/java/com/example/flixsterplus2/MediaAdapter.kt
+++ app/src/main/java/com/example/flixsterplus2/MediaAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.flixsterplus2
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.util.Log
@@ -8,30 +9,43 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.toAndroidPair
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import androidx.core.util.Pair as UtilPair
+
 
 private const val TAG = "MediaAdapter"
 class MediaAdapter(val context: Context, private val isPortrait: Boolean, private val moviesList: List<MediaItem>) :
     RecyclerView.Adapter<MediaAdapter.ViewHolder>() {
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-        val movieImageView: ImageView = itemView.findViewById(R.id.media_image_view)
-        val movieTitle: TextView = itemView.findViewById(R.id.media_title_text_view)
-        val movieDescription: TextView =
+        val mediaImageView: ImageView = itemView.findViewById(R.id.media_image_view)
+        val mediaTitle: TextView = itemView.findViewById(R.id.media_title_text_view)
+        val mediaDescription: TextView =
             itemView.findViewById(R.id.media_description_text_view)
         init {
             itemView.setOnClickListener(this)
         }
         override fun onClick(v: View?) {
-            //TODO: Add onClickListener to the ViewHolder
-            Log.d(TAG, "onClick: clicked on ${movieTitle.text}")
+
+            Log.d(TAG, "onClick: clicked on ${mediaTitle.text}")
 
             val movie = moviesList[adapterPosition]
 
-            val intent = Intent(context, MediaDetailActivity::class.java)
-            intent.putExtra("MEDIA_ITEM", movie)
-            context.startActivity(intent)
+            val intent = Intent(context, MediaDetailActivity::class.java).apply {
+                putExtra("MEDIA_ITEM", movie)
+            }
+
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                context as Activity,
+                UtilPair(itemView.findViewById<View>(R.id.media_image_view), mediaImageView.transitionName),
+                UtilPair(itemView.findViewById<View>(R.id.media_title_text_view), mediaTitle.transitionName),
+
+            )
+
+            context.startActivity(intent, options.toBundle())
 
 
         }
@@ -62,20 +76,20 @@ class MediaAdapter(val context: Context, private val isPortrait: Boolean, privat
 
     override fun onBindViewHolder(holder: MediaAdapter.ViewHolder, position: Int) {
         val movie = moviesList[position]
-        holder.movieTitle.text = movie.title
+        holder.mediaTitle.text = movie.title
 //        holder.movieDescription.text = movie.description
         if (isPortrait) {
             Glide.with(holder.itemView)
                 .load(movie.posterImageUrl)
                 .placeholder(R.drawable.placeholder_300)
                 .centerInside()
-                .into(holder.movieImageView)
+                .into(holder.mediaImageView)
         } else {
             Glide.with(holder.itemView)
                 .load(movie.backdropImageUrl)
                 .placeholder(R.drawable.placeholder_300)
                 .centerInside()
-                .into(holder.movieImageView)
+                .into(holder.mediaImageView)
         }
     }
 
--- app/src/main/java/com/example/flixsterplus2/MediaDetailActivity.kt
+++ app/src/main/java/com/example/flixsterplus2/MediaDetailActivity.kt
@@ -5,23 +5,26 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 private const val TAG = "MediaDetailActivity"
 
-class MediaDetailActivity: AppCompatActivity() {
+class MediaDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media_detail)
 
-       val mediaImageView = findViewById<ImageView>(R.id.mediaImageView)
-        val titleTextView = findViewById<TextView>(R.id.mediaTitleView)
+        val mediaImageView = findViewById<ImageView>(R.id.mediaBackdropImageView)
+        val titleTextView = findViewById<TextView>(R.id.media_title_text_view)
 //       val taglineTextView = findViewById<TextView>(R.id.taglineTextView)
-       val lastAirDateTextView = findViewById<TextView>(R.id.lastAirDateTextView)
+        val lastAirDateTextView = findViewById<TextView>(R.id.lastAirDateTextView)
         val descriptionTextView = findViewById<TextView>(R.id.media_description_text_view)
+        val mediaTempImageView = findViewById<ImageView>(R.id.mediaPosterImageView)
         // Included for future use
 //       val nextAirDateTextView = findViewById<TextView>(R.id.nextAirDateTextView)
-
+//TODO: Refactor this: getSerializableExtra is deprecated
         val media = intent.getSerializableExtra("MEDIA_ITEM") as MediaItem
 
         //TODO:Add API call for more details
@@ -34,11 +37,17 @@ class MediaDetailActivity: AppCompatActivity() {
         // Included for future use
 //        nextAirDateTextView.text = media.nextAirDate
 
+        val radius = 50 // corner radius, higher value = more rounded
         Glide.with(this)
             .load(media.backdropImageUrl)
             .placeholder(R.drawable.placeholder_300)
             .into(mediaImageView)
 
+        Glide.with(this)
+            .load(media.posterImageUrl)
+            .centerCrop()
+            .transform(RoundedCorners(radius))
+            .into(mediaTempImageView)
 
 
     }
